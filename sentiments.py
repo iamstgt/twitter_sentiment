4e152d92afc3405e1a5fb8e306c4da59a4cff81b
iamstgt <sotagoto.work@gmail.com>
2022-04-19 19:28:57 +0900

    Modified the comments

--- sentiments.py
+++ sentiments.py
@@ -2,19 +2,19 @@ import tweepy
 from flair.models import TextClassifier
 from flair.data import Sentence
 
-# Set credentials for your account
+# Set credentials
 api_key = "your-api-key"
 api_key_secret = "your-api-key-secret"
 
 access_token = "your-access-token"
 access_token_secret = "your-access-token-secret"
 
-# Authenticate you
+# Authenticate a user
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Retrieve 100 tweets on given user's timeline
+# Retrieve 200 tweets from target username
 tweets = []
 for tweet in tweepy.Cursor(api.user_timeline, screen_name='your-target-username', include_rts=False, tweet_mode="extended").items(200):
     tweets.append(tweet.full_text)
@@ -22,7 +22,7 @@ for tweet in tweepy.Cursor(api.user_timeline, screen_name='your-target-username'
 # Load model but this is memory-intensive and can take times
 sentiment_model = TextClassifier.load("en-sentiment")
 
-# Iterate through the tweets and make the prediction
+# Iterate through the tweets and make the prediction, and write them to text file
 for tweet in tweets:
     # Convert raw data into processed data
     processed_tweet = Sentence(tweet)
